c9fe067fb0177cfdf66f76c6b9233bec5b9b1a9f
Moh Noor Al Azam <noor.azam@narotama.ac.id>
2025-10-21 12:45:05 +0700

    is_prime selesai

--- agentSide/agent.py
+++ agentSide/agent.py
@@ -2,13 +2,23 @@ import sys
 import argparse
 
 def is_prime(n: int) -> bool:
-    print("Melihat apakah bilangan ", n, " adalah bilangan prima?")
+    if (n < 2):
+        return False
+    elif (n == 2):
+        return True
+    elif (n % 2 == 0):
+        return False
+    for i in range(2,n):
+        if (n % i == 0):
+            print("bisa dibagi bilangan: ", i)
+            return False
+    return True
 
 def primes_in_range(a: int, b: int):
     print("Pencari bilangan prima dari angka ", a, "sampai dengan", b)
 
 def parse_args():
-    p = argparse.ArgumentParser(description="Aplikasi mencari bilangan prima")
+    p = argparse.ArgumentParser(description="Aplikasi untuk mencari bilangan prima")
     grp = p.add_mutually_exclusive_group(required=True)
     grp.add_argument("--check", type=int, help="Periksa apakah N prima")
     grp.add_argument("--range", type=int, nargs=2, metavar=('A','B'), help="Daftar prima dalam rentang A B")
@@ -18,7 +28,11 @@ def main():
     args = parse_args()
     if args.check is not None:
         n = args.check
-        print("prima" if is_prime(n) else "bukan prima")
+        if (is_prime(n)):
+            print(n, " adalah bilangan prima")
+        else:
+            print(n, " bukan bilangan prima")
+
     elif args.range is not None:
         a, b = args.range
         if a > b:
